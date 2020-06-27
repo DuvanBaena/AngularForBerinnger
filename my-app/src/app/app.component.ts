26c9821d059f158fd343ae46e053204b3f4dbecf
duvan baena <djbpuerta@hotmail.com>
2020-06-27 15:47:16 -0500

    Fundamentos - Directiva ngFor

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-show:boolean = true;
+
+  Peoples:Array<any> = [
+    
+    {Type:'Doctor',Name:'Nelson',Age:30},
+    {Type:'Engenieer',Name :'Duvan',Age:33},
+    {Type:'Veterinarian',Name :'Nelson',Age:40}
+
+   ]
 
 }
